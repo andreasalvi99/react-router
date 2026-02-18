c81a63beb7d081d94cf63598ad5cf76d2384a09f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-18 21:00:56 +0100

    create card for products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,20 +5,33 @@ export default function Products() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get(" https://fakestoreapi.com/products").then((response) => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
       console.log(response.data);
       setProducts(response.data);
     });
   }, []);
 
   return (
-    <div className="row row-cols-4 g-4">
+    <div className="row row-cols-5 g-4">
       {products.map((product) => {
         return (
-          <div key={product.div} className="col-lg-3 col-md-6 col-sm-12 ">
-            <figure>
-              <img src={product.image} alt="" />
-            </figure>
+          <div key={product.id} className="col">
+            <div className="card h-100 d-flex flex-column">
+              <figure className="p-2 flex-grow-1 align-self-center img-container">
+                <img
+                  src={product.image}
+                  className="card-img-top product-img"
+                  alt={product.title}
+                />
+              </figure>
+
+              <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 className="card-title">{product.title}</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">&euro; {product.price}</li>
+              </ul>
+            </div>
           </div>
         );
       })}
