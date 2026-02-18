c50b8120dc914025ccd6085aedfcbef826a06379
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-18 16:50:14 +0100

    add background attachment

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -12,11 +12,13 @@ export default function Products() {
   }, []);
 
   return (
-    <div className="row row-cols-4 g-3">
+    <div className="row row-cols-4 g-4">
       {products.map((product) => {
         return (
           <div key={product.div} className="col-lg-3 col-md-6 col-sm-12 ">
-            <img src={product.image} alt="" />
+            <figure>
+              <img src={product.image} alt="" />
+            </figure>
           </div>
         );
       })}
