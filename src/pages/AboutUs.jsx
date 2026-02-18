7750fce40fca2eaa20334f9ec9e6ff74baedaa61
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-18 21:39:28 +0100

    add items in about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,8 @@
 export default function AboutUs() {
   return (
-    <>
-      <h1 className="mb-3 fw-bold">Chi Siamo</h1>
-      <p className="fs-3">
+    <div className="d-flex flex-column h-100">
+      <h1 className="mb-5 fw-bold">Chi Siamo</h1>
+      <p className="fs-3 my-5">
         Siamo un negozio di abbigliamento dedicato a chi cerca stile, qualità e
         comfort per ogni occasione. Selezioniamo con cura capi alla moda e
         sempre attuali, adatti alla vita di tutti i giorni ma anche ai momenti
@@ -11,6 +11,18 @@ export default function AboutUs() {
         proprio agio e valorizzato. Passione per la moda, attenzione ai dettagli
         e cura del cliente sono i valori che ci guidano ogni giorno.
       </p>
-    </>
+
+      <div className="container d-flex justify-content-around mt-5">
+        <div>
+          <h3>PUNTI VENDITA</h3>
+        </div>
+        <div>
+          <h3>SOCIAL</h3>
+        </div>
+        <div>
+          <h3>LAVORA CON NOI</h3>
+        </div>
+      </div>
+    </div>
   );
 }
