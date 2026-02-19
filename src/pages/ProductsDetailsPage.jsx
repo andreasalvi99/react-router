78622899a9084b6321b5364e9b4ac2dace0656c4
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-19 20:12:31 +0100

    added navigate buttons

--- src/pages/ProductsDetailsPage.jsx
+++ src/pages/ProductsDetailsPage.jsx
@@ -1,13 +1,22 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 
 export default function ProductsDetailsPage() {
   const { id } = useParams();
-  const [details, setDetails] = useState([]);
+  const [details, setDetails] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [errorAlert, setErrorAlert] = useState(false);
   const [errorInfo, setErrorInfo] = useState();
+  const navigate = useNavigate();
+
+  const prevButton = () => {
+    navigate(`/products/${parseInt(id) - 1}`);
+  };
+
+  const nextButton = () => {
+    navigate(`/products/${parseInt(id) + 1}`);
+  };
 
   const fetchProductDetail = () => {
     axios
@@ -22,13 +31,30 @@ export default function ProductsDetailsPage() {
       });
   };
 
-  useEffect(fetchProductDetail, []);
+  useEffect(fetchProductDetail, [id]);
 
   if (errorAlert)
     return (
       <div className="alert alert-danger" role="alert">
         {errorInfo}
       </div>
+    );
+
+  if (!details)
+    return (
+      <>
+        <div className="alert alert-danger" role="alert">
+          Nessun articolo disponibile
+        </div>
+        <button className="btn btn-primary">
+          <Link
+            className="text-decoration-none link-body-emphasis text-white"
+            to={"/products"}
+          >
+            Torna a Proucts
+          </Link>
+        </button>
+      </>
     );
 
   return (
@@ -40,20 +66,38 @@ export default function ProductsDetailsPage() {
           </div>
         </div>
       ) : (
-        <div className="d-flex justify-content-around bg-body-tertiary text-dark rounded">
-          <figure className="p-4">
-            <img src={details.image} alt={details.title} />
-          </figure>
-          <div className="align-self-center p-3">
-            <h5 className="mt-4">{details.title}</h5>
-            <p>{details.description}</p>
-            <p>
-              <span className="badge rounded-pill text-bg-success fs-6">
-                Rating: {" " + details.rating.rate}/5
-              </span>
-            </p>
-            <p>&euro;{details.price}</p>
+        <div className="d-flex justify-content-between align-items-center gap-4">
+          <button
+            onClick={prevButton}
+            disabled={id <= 1}
+            className="btn btn-light"
+          >
+            <i className="bi bi-arrow-left d-inline-block"></i>
+          </button>
+
+          <div className="d-flex justify-content-around bg-body-tertiary text-dark rounded">
+            <figure className="p-4">
+              <img src={details.image} alt={details.title} />
+            </figure>
+            <div className="align-self-center p-3">
+              <h5 className="mt-4">{details.title}</h5>
+              <p>{details.description}</p>
+              <p>
+                <span className="badge rounded-pill text-bg-success fs-6">
+                  Rating: {" " + details.rating.rate}/5
+                </span>
+              </p>
+              <p>&euro;{details.price}</p>
+            </div>
           </div>
+
+          <button
+            onClick={nextButton}
+            disabled={id >= 20}
+            className="btn btn-light"
+          >
+            <i className="bi bi-arrow-right d-inline-block"></i>
+          </button>
         </div>
       )}
     </>
