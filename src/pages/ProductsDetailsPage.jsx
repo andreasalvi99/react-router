cf7fd2621a10ff999c82013c639ff8e3b5aec5f8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-19 17:36:29 +0100

    set loading and after loading layout

--- src/pages/ProductsDetailsPage.jsx
+++ src/pages/ProductsDetailsPage.jsx
@@ -6,50 +6,35 @@ export default function ProductsDetailsPage() {
   const { id } = useParams();
   const [details, setDetails] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const fallBack = useNavigate();
 
   const fetchProductDetail = () => {
-    axios
-      .get(`https://fakestoreapi.com/products/${id}`)
-      .then((response) => {
-        setIsLoading(false);
-        setDetails(response.data);
-      })
-      .catch((error) => {
-        console.log(error.request.status);
-
-        setErrorAlert(true);
-        setErrorMessage(error.message);
-      });
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((response) => {
+      setIsLoading(false);
+      setDetails(response.data);
+    });
   };
 
   useEffect(fetchProductDetail, []);
 
-  //   if (errorAlert)
-  //     return (
-  //       <div className="alert alert-danger" role="alert">
-  //         {errorMessage}
-  //       </div>
-  //     );
-
   return (
     <>
-      {isLoading && (
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
+      {isLoading ? (
+        <div className="layover">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-around bg-body-tertiary text-dark rounded">
+          <figure className="p-4">
+            <img src={details.image} alt={details.title} />
+          </figure>
+          <div className="align-self-star p-3">
+            <h5 className="mt-4">{details.title}</h5>
+            <p>{details.description}</p>
+          </div>
         </div>
       )}
-      ( details ?
-      <div className="d-flex justify-content-around bg-body-tertiary text-dark rounded">
-        <figure className="p-4">
-          <img src={details.image} alt={details.title} />
-        </figure>
-        <div className="align-self-star p-3">
-          <h5 className="mt-4">{details.title}</h5>
-          <p>{details.description}</p>
-        </div>
-      </div>{" "}
-      : <div className="layover"></div>)
     </>
   );
 }
